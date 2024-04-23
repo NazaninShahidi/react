40dcf3301493cd1958c3f4cd0a9c9af776386e34
NazaninShaidi <nazaninshahidi298@gmail.com>
2024-04-23 18:37:14 +0330

    create hero-section

--- recipe-app/src/App.tsx
+++ recipe-app/src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Menu from "./components/ui/layout/Menu";
+import HeroSection from "./components/HeroSection";
 
 function App() {
   return (
     <div className="App">
       <Menu />
+      <HeroSection />
     </div>
   );
 }
