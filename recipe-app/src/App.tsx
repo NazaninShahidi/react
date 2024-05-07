64461d1705f20c02d7dc09de24dbea6ef7f25343
NazaninShaidi <nazaninshahidi298@gmail.com>
2024-05-07 21:52:01 +0330

    create about-section

--- recipe-app/src/App.tsx
+++ recipe-app/src/App.tsx
@@ -5,6 +5,8 @@ import ExploreSection from "./components/ExploreSection";
 import Footer from "./components/layout/Footer";
 import Featured from "./components/Featured";
 import RecipesFilter from "./components/recipes-filter";
+import AboutUs from "./components/about-us";
+// import CTA from "./components/cta-section";
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
       <ExploreSection />
       <Featured />
       <RecipesFilter />
+      <AboutUs />
+      {/* <CTA /> */}
       <Footer />
     </div>
   );
